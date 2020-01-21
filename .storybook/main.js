96888cdde8073bbc3c489748c930560cde118a2e
Jairo Gatjens <jairog@cr3cmam0zqf6ymd.cmass.criticalmass.com>
2020-01-21 11:27:10 -0600

    review step add circleic

---
+++ .storybook/main.js
@@ -0,0 +1,16 @@
+module.exports = {
+  stories: ["../src/**/*.stories.js"],
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-links"
+  ],
+  webpackFinal: config => {
+    config.module.rules.unshift({
+      test: /\.stories\.jsx?$/,
+      loaders: [require.resolve("@storybook/source-loader")],
+      enforce: "pre"
+    });
+    return config;
+  }
+};
